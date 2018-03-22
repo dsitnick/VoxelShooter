25cdeb268ad6e10cbb909cbf4fdc2fbc65b8fc90
Unknown <dsitnick@mosaiclearning.com>
2018-03-21 22:08:51 -0400

    Voxel classes added

---
+++ Assets/ModelTester.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelTester : MonoBehaviour {
+	public MeshFilter filter;
+
+	void Start(){
+		VoxelMesh m = new VoxelMesh (3, 3, 3, 1);
+		m.Set (0, 0, 0, 0);
+		m.Set (1, 0, 0, 1);
+		m.Set (2, 0, 0, 2);
+		m.Set (0, 1, 0, 3);
+		m.Set (2, 1, 0, 4);
+		m.Set (0, 0, 1, 5);
+		m.Set (1, 1, 1, 6);
+		m.Set (2, 2, 2, 7);
+
+		filter = GetComponent<MeshFilter> ();
+		filter.mesh = m.ToMesh ();
+	}
+}
---
+++ Assets/Scripts/Voxel/VoxelColors.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelColors {
+	public static Color[] COLORS = new Color[]{
+		Color.red,
+		Color.yellow,
+		Color.green,
+		Color.cyan,
+		Color.blue,
+		Color.magenta,
+		Color.black,
+		Color.white
+	};
+}
